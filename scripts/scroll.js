5fc6a46afdf9c7cb4088ee615fd208175d90a493
ahci21KS <70646505+ahci21KS@users.noreply.github.com>
2020-09-11 13:04:02 -0500

    MediaQueries Features
    -Added Media Queries

--- scripts/scroll.js
+++ scripts/scroll.js
@@ -4,7 +4,11 @@ var currentScrollPos = window.pageYOffset;
   if (prevScrollpos > currentScrollPos) {
     document.getElementById("navbar").style.top = "0";
   } else {
-    document.getElementById("navbar").style.top = "-50px";
+    if(window.screen.width==1980){      
+      document.getElementById("navbar").style.top = "-150px";
+    }else{
+      document.getElementById("navbar").style.top = "-50px";
+    }
   }
   prevScrollpos = currentScrollPos;
 }
